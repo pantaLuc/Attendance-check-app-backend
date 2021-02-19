68264bd002d1b0f7d0ef8d07281cd7a821af9ab7
joan loic <ngounouloic675@gmail.com>
2021-02-19 13:30:50 +0100

    signup implementer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import exceptions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['post'])
+def signup(request):
+    data = request.data
+    if data['password'] != data["password_confirm"]:
+        raise exceptions.APIException("le mot de passe ne convient pas")
+    serializer = UserSerializer(data = data)
+    serializer.is_valid(raise_exception = True)
+    serializer.save()
+    return Response(serializer.data)
+
