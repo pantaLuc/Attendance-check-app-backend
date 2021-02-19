2a68840e65503e832ece1239f26540b9b0f6e376
joan loic <ngounouloic675@gmail.com>
2021-02-19 12:04:53 +0100

    creation user models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastnane = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=255)
