bbc29d0cb4a4cc7b841e2444addad7a863180d0e
joan loic <ngounouloic675@gmail.com>
2021-02-19 14:29:14 +0100

    corrections des bugs sur models users1

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class User(models.Model):
     first_name = models.CharField(max_length=255)
-    last_nane = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
     phone = models.CharField(max_length=15)
     password = models.CharField(max_length=255)
