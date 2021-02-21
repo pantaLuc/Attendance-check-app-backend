b71d6f2de5cedcbe510d2c788dd94b1adf33158a
pantaLuc <lucapameni@gmail.com>
2021-02-21 09:32:24 +0100

    Update Profile and password possible

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from .serializers import UsersSerializer, PermissionSerializer, RoleSerializer
 from .models import User, Permission, Role
 from .authentication import access_tokens, JwtAuthenticatedUser
+from .permissions import ViewPermission
 
 
 @api_view(['post'])
