856aa516b3a9403ec88272e6394d8220c32f831f
DelRoos <delanoroosvelt733@gmail.com>
2021-03-02 15:12:57 +0100

    Ajouts des data pour les donnéesdu backend

--- decanatsurveillance/settings.py
+++ decanatsurveillance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jt49$5yqgcr*qjmgzmz5bwc)-9(zc=mju2^p_0vnyz&j9rb^@='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.100"]
 
 
 # Application definition
