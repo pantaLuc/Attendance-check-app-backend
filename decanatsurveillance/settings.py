3d12149cab2b3c3700ccfd08f3115e4e010f0a9e
Luc Perin Panta <45631330+pantaLuc@users.noreply.github.com>
2021-03-07 10:18:40 +0100

    Update settings.py

--- decanatsurveillance/settings.py
+++ decanatsurveillance/settings.py
@@ -148,4 +148,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'jwt',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
